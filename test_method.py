3166d406f1ed421cdef27fa7c344a6c86cce3f3d
yiming-l21 <liuyiming5117@163.com>
2024-07-12 17:05:57 +0800

    add inverse transform sampling:lym

---
+++ test_method.py
@@ -0,0 +1,56 @@
+# ==========================================
+# test_method.py
+# Description: Test the watermark algorithm
+# ==========================================
+
+import json
+import torch
+from watermark.auto_watermark import AutoWatermark
+from utils.transformers_config import TransformersConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+
+# Load data
+with open('dataset/c4/processed_c4.json', 'r') as f:
+    lines = f.readlines()
+item = json.loads(lines[0])
+prompt = item['prompt']
+natural_text = item['natural_text']
+
+
+def test_algorithm(algorithm_name):
+    # Check algorithm name
+    assert algorithm_name in ['KGW', 'Unigram', 'SWEET', 'EWD', 'SIR', 'XSIR', 'UPV', 'EXP', 'EXPEdit','ITSEdit']
+
+    # Device
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    # Transformers config
+    transformers_config = TransformersConfig(model=AutoModelForCausalLM.from_pretrained("/data2/shared_model/facebook/opt-1.3b/").to(device),
+                                            tokenizer=AutoTokenizer.from_pretrained("/data2/shared_model/facebook/opt-1.3b/"),
+                                            vocab_size=50272,
+                                            device=device,
+                                            max_new_tokens=200,
+                                            min_length=230,
+                                            do_sample=True,
+                                            no_repeat_ngram_size=4)
+    
+    # Load watermark algorithm
+    myWatermark = AutoWatermark.load(f'{algorithm_name}', 
+                                     algorithm_config=f'config/{algorithm_name}.json',
+                                     transformers_config=transformers_config)
+
+    watermarked_text = myWatermark.generate_watermarked_text(prompt)
+    unwatermarked_text = myWatermark.generate_unwatermarked_text(prompt)
+    detect_result = myWatermark.detect_watermark(watermarked_text)
+    print(detect_result)
+    detect_result = myWatermark.detect_watermark(unwatermarked_text)
+    print(detect_result)
+    detect_result = myWatermark.detect_watermark(natural_text)
+    print(detect_result)
+
+
+if __name__ == '__main__':
+    test_algorithm('ITSEdit')
+
+
