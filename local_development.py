0df2a527457a52b865eaeb331986ef24b137ed95
vi-mcw196 <moldlova50@gmail.com>
2024-11-07 18:36:20 +0100

    Proof of Concept

--- local_development.py
+++ local_development.py
@@ -2,11 +2,6 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from utils.transformers_config import TransformersConfig
-from visualize.color_scheme import ColorSchemeForDiscreteVisualization
-from visualize.font_settings import FontSettings
-from visualize.legend_settings import DiscreteLegendSettings
-from visualize.page_layout_settings import PageLayoutSettings
-from visualize.visualizer import DiscreteVisualizer
 from watermark.auto_watermark import AutoWatermark
 
 # Load watermark algorithm
@@ -27,29 +22,28 @@ myWatermark = AutoWatermark.load('KGW',
 # Prompt
 prompt = 'Good Morning.'
 
-# Generate and detect
+# Generate text
 watermarked_text = myWatermark.generate_watermarked_text(prompt)
 unwatermarked_text = myWatermark.generate_unwatermarked_text(prompt)
 
-# Get data for visualization
-watermarked_data = myWatermark.get_data_for_visualization(watermarked_text)
-unwatermarked_data = myWatermark.get_data_for_visualization(unwatermarked_text)
+# Detect
+detect_result1 = myWatermark.detect_watermark(watermarked_text)
+detect_result2 = myWatermark.detect_watermark(unwatermarked_text)
+detect_result3 = myWatermark.detect_watermark(prompt)
 
-# Init visualizer
-visualizer = DiscreteVisualizer(color_scheme=ColorSchemeForDiscreteVisualization(),
-                                font_settings=FontSettings(),
-                                page_layout_settings=PageLayoutSettings(),
-                                legend_settings=DiscreteLegendSettings())
-# Visualize
-watermarked_img = visualizer.visualize(data=watermarked_data,
-                                       show_text=True,
-                                       visualize_weight=True,
-                                       display_legend=True)
+print("LLM-generated watermarked text:")
+print(watermarked_text)
+print('\n')
+print(detect_result1)
+print('\n')
 
-unwatermarked_img = visualizer.visualize(data=unwatermarked_data,
-                                         show_text=True,
-                                         visualize_weight=True,
-                                         display_legend=True)
-# Save
-watermarked_img.save("KGW_watermarked.png")
-unwatermarked_img.save("KGW_unwatermarked.png")
+print("LLM-generated unwatermarked text:")
+print(unwatermarked_text)
+print('\n')
+print(detect_result2)
+print('\n')
+
+print("Natural text:")
+print(prompt)
+print('\n')
+print(detect_result3)
